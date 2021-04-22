5d75407c7591379d8846655083e12f87c9093abe
Zach T <ztrokey@gmail.com>
2021-04-22 17:24:33 -0600

    add generator and assigner methods and tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,9 @@
 class Enigma
+  attr_reader :characters
 
   def initialize
+    @characters = ("a".."z").to_a << " "
+
   end
 
   def encrypt(string, key, date)
@@ -9,12 +12,35 @@ class Enigma
   def decrypt(string, key, date)
   end
 
-  def key_generator
-    '%05d' % rand(5 ** 5)
+  def generate_key
+    key = '02715'
+    # key = '%05d' % rand(5 ** 5) this does the random
+    key.chars.map(&:to_i)
   end
 
-  def offset_generator(date)
-    integer = date.to_i ** 2
-    integer.to_s.split(//).last(4).join
+  def offset_generator
+    date = '040895' #change this to Time.now.strftime("%d%m%Y")
+    (date.to_i ** 2).to_s.split(//).last(4)
+  end
+
+  # def offset_generator(date)
+  #   integer = date.to_i ** 2
+  #   integer.to_s.split(//).last(4)
+  # end
+
+  def a_shift_assigner
+    generate_key[0..1].join.to_i + offset_generator[0].to_i
+  end
+
+  def b_shift_assigner
+    generate_key[1..2].join.to_i + offset_generator[1].to_i
+  end
+
+  def c_shift_assigner
+    generate_key[2..3].join.to_i + offset_generator[2].to_i
+  end
+
+  def d_shift_assigner
+    generate_key[3..4].join.to_i + offset_generator[3].to_i
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,11 +12,13 @@ RSpec.describe Enigma do
     end
   end
 
-  describe '#key_generator' do
-    xit 'generates a random 5 digit number' do
+  describe '#generate_key' do
+    it 'generates a random 5 digit number' do
       enigma = Enigma.new
 
-      expect(enigma.key_generator).to eq('')
+
+      expect(enigma.generate_key).to eq([0, 2, 7, 1, 5])
+      # expect(enigma.key_generator).to eq('')
     end
   end
 
@@ -24,7 +26,21 @@ RSpec.describe Enigma do
     it 'generates a number based off of date' do
       enigma = Enigma.new
 
-      expect(enigma.offset_generator('040895')).to eq('1025')
+      expect(enigma.offset_generator).to eq(["1", "0", "2", "5"])
+      # expect(enigma.offset_generator('040895')).to eq(["1", "0", "2", "5"])
+    end
+  end
+
+  describe '#shift_assigners' do
+    it 'assigns the shifts' do
+      enigma = Enigma.new
+      enigma.generate_key
+      enigma.offset_generator
+      # require 'pry'; binding.pry
+      expect(enigma.a_shift_assigner).to eq(3)
+      expect(enigma.b_shift_assigner).to eq(27)
+      expect(enigma.c_shift_assigner).to eq(73)
+      expect(enigma.d_shift_assigner).to eq(20)
     end
   end
 end
